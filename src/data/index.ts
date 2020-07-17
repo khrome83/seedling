911cfed6abc23279811dbe3b9dd6adcf69edcb7a
Zane Milakovic <zane@voltron.attlocal.net>
2020-07-17 09:08:51 -0500

    changes to request, response pattern

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,50 +1,147 @@
 import { v5 } from "https://deno.land/std/uuid/mod.ts";
+import { Identifier } from "https://deno.land/x/dash/util.ts";
+import cache from "./cache.ts";
 
 const namespace = "f6360cb2-cdac-4d8d-a269-a5f65b054128";
 
-export interface Attributes {
-  [name: string]: string;
+export interface Skip {
+  type: "SKIP";
 }
+
+const skip = (): Skip => ({
+  type: "SKIP",
+});
+
+export interface End {
+  type: "END";
+}
+
+const end = (): End => ({
+  type: "END",
+});
+
+export interface Retry {
+  type: "RETRY";
+  msg: string;
+}
+
+const retry = (msg: string): Retry => ({
+  type: "RETRY",
+  msg,
+});
+
+export interface Error {
+  type: "ERROR";
+  msg: string;
+  stack?: TypeError;
+}
+
+const error = (msg: string, stack?: TypeError): Error => ({
+  type: "ERROR",
+  msg,
+  stack,
+});
+
+export interface Success {
+  type: "SUCCESS";
+  response: object;
+}
+
+const success = (response: object): Success => ({
+  type: "SUCCESS",
+  response,
+});
+
+export interface Response {
+  skip: Function;
+  end: Function;
+  retry: Function;
+  error: Function;
+  success: Function;
+}
+
+const response: Response = {
+  skip,
+  end,
+  retry,
+  error,
+  success,
+};
+
+export interface Request {
+  attrs?: object;
+  body?: string;
+}
+
+const buildRequest = (attrs: object, body: string): Request => ({
+  attrs,
+  body,
+});
 
 export const data = async (
   processor: string,
-  attrs: Attributes,
+  attrs: object,
   body: string = "",
   root: string = Deno.cwd()
 ): Promise<object> => {
-  // Generate Cache Key
+  // Generate Cache Key (v5 UUID)
   const cacheKeyOptions = {
     value: JSON.stringify({ processor, attrs, body }),
     namespace,
   };
-  const cacheKey = v5.generate(cacheKeyOptions);
+  const cacheKey = v5.generate(cacheKeyOptions) as Identifier;
 
-  // Need Cache
-  // Check Cache
-  // No Found - Make Request
-  // Push to Cache
-  // Return
-  // Can't return promise... must await results?
+  // Determine correct path to process under
+  const tsPath = `${root}/data/${processor}.ts`;
+  const jsPath = `${root}/data/${processor}.js`;
+  const importPath = await Deno.lstat(tsPath)
+    .then(() => {
+      return tsPath;
+    })
+    .catch(async () => {
+      return await Deno.lstat(jsPath)
+        .then(() => {
+          return jsPath;
+        })
+        .catch((e) => {
+          throw new Error(
+            "Invalid use argument for data directive, file not found"
+          );
+        });
+    });
 
-  // Need to create Error Handler
-  // Need to create data structure for
-  // - SKIP
-  // - ERROR
-  // - END
-  // - DATA
-  // Meta Data - Cache Hit vs Cache Miss?
+  // Either return from Cache or Request New Data
+  if (!cache.has(cacheKey)) {
+    try {
+      const dataProcessor = await import(importPath);
+      const result = await dataProcessor.default(
+        buildRequest(attrs, body),
+        response
+      );
 
-  // Create Cache Key - UUID
-  try {
-    // Check if file exists
-    // Error if file does not exist
-    // Check if TS or JS extension (magic auto resolution)
-    const dataProcessor = await import(`${root}/data/${processor}.ts`);
-    const result = await dataProcessor.default(attrs, body);
+      // Retries
+      // Need to create Error Handler
+      // Need to create data structure for
+      // - SKIP
+      // - ERROR
+      // - END
+      // - DATA
+      // - RETRY
+      // Or do we not....
 
-    // TODO: Insert Into Cache using cacheKey
-    return result;
-  } catch (e) {
-    throw e;
+      cache.set(cacheKey, result);
+      return {
+        ...result,
+        meta: { cacheHit: false, cacheKey },
+      };
+    } catch (e) {
+      throw e;
+    }
+  } else {
+    return {
+      type: "DATA",
+      response: cache.get(cacheKey),
+      meta: { cacheHit: true, cacheKey },
+    };
   }
 };
