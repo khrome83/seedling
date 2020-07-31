49772d796d20d8a23b81c15d2d335d7a45897dc3
Zane Milakovic <zane@voltron.attlocal.net>
2020-07-31 17:31:40 -0500

    removes uneeded comments

--- src/benchmarks/benchmarks.ts
+++ src/benchmarks/benchmarks.ts
@@ -15,29 +15,6 @@ import {
 import "./parser.ts";
 import "./serializer.ts";
 
-// , {
-//   title: "Example Markdown",
-//   description: (runResult: BenchmarkRunResult) =>
-//     `This markdown was generated with the use of \`prettyBenchmarkDown\`.\nIf you use a function for the \`description\` or \`afterTables\`, you can process the results here as well: \n\n > In this benchmark ${runResult.results.length} benches were run.`,
-//   afterTables:
-//     "\n---\n\nThis is the `afterTables`. This behaves the same as `description`, it just puts this at the bottom of the markdown.\nHere its defined with a simple string.\n\nCheck out the Github Action, which comments a markdown like this on PRs: $link",
-//   groups: [
-//     {
-//       include: /array/,
-//       name: "Default columns and dynamic text",
-//       description:
-//         "This is a group's `description`.\nHere you can see what the default columns are, and how you can use a `function` as `description` or `afterTable` inside a group",
-//       afterTable: (
-//         gr: BenchmarkResult[],
-//         g: GroupDefinition,
-//         rr: BenchmarkRunResult
-//       ) =>
-//         `This is a group's \`afterTable\`.\nHere you can access eg. the group name: \`${g.name}\`, benchmarks in this group: \`${gr.length}\` of them here, or the whole \`BenchmarkRunResult\`: \`${rr.results.length}\` benchmarks total`,
-//       columns: [...defaultColumns],
-//     },
-//   ],
-// }
-
 if (Deno.env.get("CI")) {
   // CI Specific Version
   runBenchmarks({ silent: true })
