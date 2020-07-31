ee9469ba88f17801faffa6e1f07dd0bbe128c651
Zane Milakovic <zane@voltron.attlocal.net>
2020-07-31 12:55:51 -0500

    uses config to determine legacy support

--- src/dict/voidElements.ts
+++ src/dict/voidElements.ts
@@ -1,5 +1,7 @@
+import config from "../config/index.ts";
+
 // Void Elements are a Subset of HTML elements that do not have a Solidus and never have a closing tag.
-export default new Set([
+const standard = new Set([
   "area",
   "base",
   "br",
@@ -15,3 +17,33 @@ export default new Set([
   "track",
   "wbr",
 ]) as Set<string>;
+
+// Void Elements are a Subset of HTML elements that do not have a Solidus and never have a closing tag.
+// A superset of valid void elements that include obsolete and deprecated elements
+const legacy = new Set([
+  "area",
+  "base",
+  "basefont",
+  "bgsound",
+  "br",
+  "col",
+  "command",
+  "embed",
+  "frame",
+  "hr",
+  "image",
+  "img",
+  "input",
+  "isindex",
+  "keygen",
+  "link",
+  "menuitem",
+  "meta",
+  "nextid",
+  "param",
+  "source",
+  "track",
+  "wbr",
+]) as Set<string>;
+
+export default !config.legacy ? standard : legacy;
